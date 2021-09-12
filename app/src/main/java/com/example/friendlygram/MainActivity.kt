689213d0e15415a39efb3be4d678b9361c428601
DT5Gen <marinovod@gmail.com>
2021-09-12 05:59:18 +0300

    DockSide v0.3.62

--- app/src/main/java/com/example/friendlygram/MainActivity.kt
+++ app/src/main/java/com/example/friendlygram/MainActivity.kt
@@ -2,10 +2,71 @@ package com.example.friendlygram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CancellationSignal
+import androidx.appcompat.widget.Toolbar
+import com.example.friendlygram.databinding.ActivityMainBinding
+import com.mikepenz.materialdrawer.AccountHeader
+import com.mikepenz.materialdrawer.AccountHeaderBuilder
+import com.mikepenz.materialdrawer.Drawer
+import com.mikepenz.materialdrawer.DrawerBuilder
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem
+import java.util.function.Consumer
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mBinding: ActivityMainBinding
+    private lateinit var mDrawer: Drawer
+    private lateinit var mHeader: AccountHeader
+    private lateinit var mToolbar: Toolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(mBinding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        initFields()
+        initFunc()
+    }
+
+    private fun initFunc() {
+        setSupportActionBar(mToolbar)
+        createHeader()
+        createDrawer()
+    }
+
+    private fun createDrawer() {
+        mDrawer = DrawerBuilder()
+            .withActivity(this)
+            .withToolbar(mToolbar)
+            .withActionBarDrawerToggle(true)
+            .withSelectedItem(-1)
+            .withAccountHeader(mHeader)
+            .addDrawerItems(
+                PrimaryDrawerItem().withIdentifier(100)
+                    .withIconTintingEnabled(true)
+                    .withName("Создать группу")
+                    .withSelectable(false)
+
+            ).build()
+    }
+
+    private fun createHeader() {
+mHeader = AccountHeaderBuilder()
+    .withActivity(this)
+    .withHeaderBackground(R.drawable.header)
+    .addProfiles(
+        ProfileDrawerItem().withName("COINGEN")
+            .withEmail("+79111111111")
+    ).build()
+    }
+
+    private fun initFields(){
+        mToolbar = mBinding.mainTollbar
+
     }
 }
