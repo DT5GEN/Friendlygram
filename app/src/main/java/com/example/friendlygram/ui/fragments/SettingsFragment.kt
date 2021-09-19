7d2a1fd359aa1e123e0f9ade5dcce9ea4b7415e2
DT5Gen <marinovod@gmail.com>
2021-09-20 00:43:29 +0300

    RefactorMenuSettings v0.7.62.d

--- app/src/main/java/com/example/friendlygram/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/friendlygram/ui/fragments/SettingsFragment.kt
@@ -1,5 +1,7 @@
 package com.example.friendlygram.ui.fragments
 
+import android.view.Menu
+import android.view.MenuInflater
 import com.example.friendlygram.R
 
 
@@ -8,6 +10,12 @@ class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
 
     override fun onResume() {
         super.onResume()
+        setHasOptionsMenu(true)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        activity?.menuInflater?.inflate(R.menu.settings_action_menu, menu)
     }
+
+
+}
