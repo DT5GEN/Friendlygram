e05c3ff4e586f895b715998f5e8bb97ae8ffb6dc
DT5Gen <marinovod@gmail.com>
2021-09-28 23:56:57 +0300

    addReadContacts_v0.28.62.d

--- app/src/main/java/com/example/friendlygram/MainActivity.kt
+++ app/src/main/java/com/example/friendlygram/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.friendlygram
 
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 import com.example.friendlygram.activities.RegisterActivity
 import com.example.friendlygram.databinding.ActivityMainBinding
 import com.example.friendlygram.models.User
@@ -32,10 +34,18 @@ class MainActivity : AppCompatActivity() {
         APP_ACTIVITY = this
         initFirebase()
         initUser {
+
+            initContacts()
             initFields()
             initFunc()
         }
 
+    }
+
+    private fun initContacts() {
+        if(checkPermission(READ_CONTACTS)){
+            showToast("Чтение контактов")
+        }
     }
 
 
@@ -73,5 +83,16 @@ class MainActivity : AppCompatActivity() {
     override fun onStop() {
         super.onStop()
         AppStates.updateState(AppStates.OFFLINE)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (ContextCompat.checkSelfPermission(APP_ACTIVITY, READ_CONTACTS)==PackageManager.PERMISSION_GRANTED){
+            initContacts()
+        }
     }
 }
